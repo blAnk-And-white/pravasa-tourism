5065a037a72861e440af09d1bb5036b55c38560d
blAnk-And-white <dhavalchavda1090@gmail.com>
2025-12-14 22:16:13 +0530

    Added all program files

---
+++ Python Project/hello/home/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from home import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('packages/', views.packages, name='packages'),
+    path('contact/', views.contact, name='contact'),
+    path('destinations/', views.destinations, name='destinations'),
+    
+]
---
+++ Python Project/hello/home/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from home.models import Contact
+
+def index(request):
+    context = {
+        'variable' : "jeeee"
+    }
+    return render(request, 'index.html', context)
+
+def packages(request):
+    return render(request, 'packages.html',)
+
+def destinations(request):
+    return render(request, 'destinations.html',)
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+        contact.save()
+    return render(request, 'contact.html',)
+
+
+def about(request):
+    return render(request, 'about.html',)
+
+# Create your views here.
